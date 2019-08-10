9c7a73ba1ec762d0fdc3956be22e4a8995a3f00d
Michael Sachs <micsac@kep.local>
2019-08-10 17:52:36 +0200

    split variance estimation off into vcov and implement other variance methods

--- R/cumincglm.R
+++ R/cumincglm.R
@@ -18,10 +18,18 @@
 #'
 cumincglm <- function(formula, time, cause = "1", link = "identity", data, ...) {
 
-    marginal.estimate <- prodlim::prodlim(update.formula(formula, . ~ 1), data = data)
-    newdata <- data
 
-    newdata[["pseudo.vals"]] <- c(prodlim::jackknife(marginal.estimate, times = time, cause = cause))
+    stopifnot(length(time) == 1)
+
+    marginal.estimate <- prodlim::prodlim(update.formula(formula, . ~ 1), data = data)
+
+    newdata <- do.call(rbind, lapply(1:length(time), function(i) data))
+
+    jackk <- prodlim::jackknife(marginal.estimate, times = time, cause = cause)
+
+    newdata[["pseudo.vals"]] <- c(jackk)
+    newdata[["pseudo.time"]] <- rep(time, each = nrow(jackk))
+
     if(marginal.estimate$model == "survival") newdata[["pseudo.vals"]] <- 1 - newdata[["pseudo.vals"]]
 
     newdata[["startmu"]] <- rep(mean(newdata$pseudo.vals), nrow(newdata))
@@ -42,154 +50,20 @@ cumincglm <- function(formula, time, cause = "1", link = "identity", data, ...) 
         )
 
     } else {
-        datamat <- cbind(marginal.estimate$model.response[, "time"],
-                         marginal.estimate$model.response[, "status"] != 0, ## not censored indicator
-                         c(attr(marginal.estimate$model.response, "states"), "0")[marginal.estimate$model.response[, "event"]] == cause,
-                         marginal.estimate$model.response[, "status"] == 0 ## censored indicator
-        )
-    }
 
-    noobs <- len <- nrow(datamat)
+        dmatframe <- as.matrix(model.frame(update.formula(formula, .~1), data = newdata)[, 1])
+        colnames(dmatframe) <- c("time", "status", "event")
 
-    datord <- order(datamat[, 1], -datamat[, 2], -datamat[, 3], -datamat[, 4])
-    datamat <- datamat[datord, ]
-
-    datain <- datamat[datamat[, 1] <= time, ]
-
-    len2 <- nrow(datain)
-
-    timejump <- datain[1:(len2 - 1), 1] != datain[2:len2, 1]
-    times_id <- c(which(timejump == TRUE), len2)
-
-    n_times <- length(times_id)
-    times_nr <- cumsum(c(1, timejump))
-
-    Y_lag <- (noobs:1)[times_id]
-
-    N_all <- datain[, 2]
-    N_1 <- datain[, 3]
-    N_0 <- datain[, 4]
-
-    # Calculating the overall survival function (missing values mean a population die out and should be interpreted as 0)
-    H_0 <- cumsum(N_0)/noobs
-    H_0_dif = c(H_0[times_id[1]],  ((H_0[times_id])[2:n_times] - (H_0[times_id])[1:(n_times-1)]))
-
-
-    H_1 = cumsum(N_1)/noobs
-    H_1_dif = c(H_1[times_id[1]] , ((H_1[times_id])[2:n_times] - (H_1[times_id])[1:(n_times-1)]))
-
-    H_all = cumsum(N_all)/noobs
-    H_all_dif = c(H_all[times_id[1]] , ((H_all[times_id])[2:n_times] - (H_all[times_id])[1:(n_times-1)]))
-
-    H_lag = Y_lag / noobs
-    H_lag_inv = 1 / H_lag
-    H_lag_inv[is.na(H_lag_inv)] <- 0
-
-
-    Lambda_0_dif = H_0_dif / H_lag
-    Lambda_1_dif = H_1_dif / H_lag
-    Lambda_all_dif = H_all_dif / H_lag
-    Lambda_0_dif_comp_inv = 1 / (1 - Lambda_0_dif)
-    Lambda_0_dif_comp_inv[is.na(Lambda_0_dif_comp_inv)] <- 0
-
-    S = exp(cumsum(log(1 - Lambda_all_dif)))
-    S[is.na(S)] <- 0
-    S_lag = c(1 , S[1:(n_times-1)])
-
-    G = exp(cumsum(log(1 - Lambda_0_dif)))
-    G[is.na(G)] <- 0
-    G_lag = c(1 , G[1:(n_times-1)])
-    G_lag_inv = 1 / G_lag
-    G_lag_inv[is.na(G_lag_inv)] <- 0
-
-    F_1 = cumsum(S_lag * Lambda_1_dif)
-
-    d_phi_1 = c((N_1 * G_lag_inv[times_nr]) , rep(0, noobs - len2))
-    d_phi_2 = c(N_0 * (F_1[n_times] - F_1[times_nr]) * Lambda_0_dif_comp_inv[times_nr] * H_lag_inv[times_nr] -
-                    cumsum(H_0_dif * (F_1[n_times] - F_1) * Lambda_0_dif_comp_inv * H_lag_inv^2)[times_nr] ,
-                rep(-sum(H_0_dif * (F_1[n_times] - F_1) * Lambda_0_dif_comp_inv * H_lag_inv^2), noobs-len2))
-
-    beta = fit.lin$coefficients
-    k = length(beta)
-
-    z = fit.lin$x[datord,]  ## what if the model contains po at multiple time points?
-
-    muhat = fit.lin$family$linkinv(z %*% beta)
-    Ahat = z * as.vector(fit.lin$family$mu.eta(z %*% beta))
-    Ahat_red = Ahat[1:len2,]
-    mu_derivhat = z * as.vector(fit.lin$family$mu.eta(z %*% beta))
-
-    a_len <- a1_len <- a2_len <- a3_len <-
-        b_1 <- b_2 <- b_3 <- b_4 <- b_5 <- b_6 <-
-        matrix(NA, nrow = noobs, ncol = k)
-    H_0z <- H_0z_dif <- H_1z <- H_1z_dif <- H_z <- H_z_lag <- matrix(NA, nrow = len2, ncol = k)
-
-
-    ## compute variance
-    for(j in 1:k) {
-        a_len[,j] = Ahat[,j] * (d_phi_1-muhat+d_phi_2)
-        a1_len[,j] = Ahat[,j]  * (d_phi_1)
-        a2_len[,j] = Ahat[,j] * (-muhat)
-        a3_len[,j] = Ahat[,j] * (d_phi_2)
-
-        H_0z[,j] = cumsum(Ahat_red[,j] * N_0)/noobs
-        H_0z_dif[,j] = c(H_0z[times_id[1],j] , ((H_0z[times_id,j])[2:n_times] - (H_0z[times_id,j])[1:(n_times-1)]))
-
-        H_1z[,j] = cumsum(Ahat_red[,j] * N_1)/noobs
-        H_1z_dif[,j] = c(H_1z[times_id[1],j] , ((H_1z[times_id,j])[2:n_times] - (H_1z[times_id,j])[1:(n_times-1)]))
-
-        H_z[,j] = (sum(Ahat[,j]) - cumsum(Ahat_red[,j]))/noobs
-        H_z_lag[,j] = c(sum(Ahat[,j]) / noobs , (H_z[1:(len2-1),j])[times_id[1:(n_times-1)]])
-
-        temp1 = cumsum( H_0z_dif[,j] * H_lag_inv * Lambda_0_dif_comp_inv - H_z_lag[,j] * H_lag_inv^2 * Lambda_0_dif_comp_inv * H_0_dif )
-        temp1_lag = c(0 , temp1[1:(n_times-1)])
-        temp1_dif = temp1 - temp1_lag
-
-        temp2 = cumsum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv * Lambda_0_dif_comp_inv -
-                            H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv  * H_0_dif )
-        temp3 = cumsum( H_1z_dif[,j] / G_lag )
-
-
-        b_1[,j] = c(N_1 * G_lag_inv[times_nr] * temp1_lag[times_nr] , rep(0, len-len2))
-
-        b_2[,j] = c(N_0 * (temp3[n_times] - temp3[times_nr]) * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
-                        cumsum( H_0_dif * (temp3[n_times] - temp3) * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
-                    rep(-sum( H_0_dif * (temp3[n_times] - temp3) * H_lag_inv^2 * Lambda_0_dif_comp_inv ), len-len2))
-
-        temp4 = cumsum( H_1_dif * G_lag_inv * temp1 )
-
-        b_3[,j] = c(N_0 * (temp4[n_times] - temp4[times_nr]) * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
-                        cumsum( H_0_dif * (temp4[n_times] - temp4) * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
-                    rep(-sum( H_0_dif * (temp4[n_times] -temp4) / H_lag^2 / (1-Lambda_0_dif) ), len-len2 ))
-
-
-        b_4[,j] = c(- N_0 * (F_1[n_times] - F_1[times_nr]) * H_z_lag[times_nr,j] * H_lag_inv[times_nr]^2 * Lambda_0_dif_comp_inv[times_nr] +
-                        cumsum( H_0_dif * (F_1[n_times] - F_1) * H_z_lag[,j] * H_lag_inv^3 * Lambda_0_dif_comp_inv )[times_nr] ,
-                    rep(sum( H_0_dif * (F_1[n_times] - F_1) * H_z_lag[,j] * H_lag_inv^3 * Lambda_0_dif_comp_inv ), len-len2 ))
-
-
-        b_5[,j] = c(-cumsum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv -
-                                 H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^3 * Lambda_0_dif_comp_inv * H_0_dif )[times_nr] ,
-                    rep(- sum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv -
-                                   H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^3 * Lambda_0_dif_comp_inv * H_0_dif ), len-len2 ))
-
-        b_6[,j] = c(N_0 * (F_1[n_times] - F_1[times_nr]) * temp1_dif[times_nr] * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
-                        cumsum( H_0_dif * (F_1[n_times] - F_1) * temp1_dif * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
-                    rep(-sum( H_0_dif * (F_1[n_times] - F_1) * temp1_dif * H_lag_inv^2 * Lambda_0_dif_comp_inv ), len-len2 ))
-
+        datamat <- cbind(dmatframe[, "time"],
+              dmatframe[, "status"] != 0,
+              dmatframe[, "event"] == as.numeric(cause),
+              dmatframe[, "status"] == 0)
 
     }
 
-    b <- b_1 + b_2 + b_3 + b_4 + b_5 + b_6
-    Sigma <- t(a_len) %*% a_len / noobs + t(a_len) %*% b / noobs +
-        t(b) %*% a_len / noobs + t(b) %*% b / noobs
 
-
-    Minvhat <- solve(t(Ahat) %*% mu_derivhat / nrow(Ahat))
-
-    ovg.vcov <- Minvhat %*% Sigma %*% t(Minvhat) / noobs
-
-    fit.lin$corrected.vcov <- ovg.vcov
+#    fit.lin$corrected.vcov <- ovg.vcov
+    fit.lin$datamat <- datamat
     fit.lin$time <- time
     fit.lin$cause <- cause
     fit.lin$link <- link
@@ -231,10 +105,168 @@ print.pseudoglm <- function (x, digits = max(3L, getOption("digits") - 3L), ...)
 
 
 #' Vcov method
+#'
+#' @param type one of "corrected", "robust", "naive"
+#'
 #' @export
-vcov.pseudoglm <- function(object, complete = TRUE, ...) {
+vcov.pseudoglm <- function(object, type = "corrected", ...) {
 
-    object$corrected.vcov
+    if(type == "corrected") {
+
+        datamat <- object$datamat
+        noobs <- len <- nrow(datamat)
+
+        datord <- order(datamat[, 1], -datamat[, 2], -datamat[, 3], -datamat[, 4])
+        datamat <- datamat[datord, ]
+
+        datain <- datamat[datamat[, 1] <= object$time, ]
+
+        len2 <- nrow(datain)
+
+        timejump <- datain[1:(len2 - 1), 1] != datain[2:len2, 1]
+        times_id <- c(which(timejump == TRUE), len2)
+
+        n_times <- length(times_id)
+        times_nr <- cumsum(c(1, timejump))
+
+        Y_lag <- (noobs:1)[times_id]
+
+        N_all <- datain[, 2]
+        N_1 <- datain[, 3]
+        N_0 <- datain[, 4]
+
+        # Calculating the overall survival function (missing values mean a population die out and should be interpreted as 0)
+        H_0 <- cumsum(N_0)/noobs
+        H_0_dif = c(H_0[times_id[1]],  ((H_0[times_id])[2:n_times] - (H_0[times_id])[1:(n_times-1)]))
+
+
+        H_1 = cumsum(N_1)/noobs
+        H_1_dif = c(H_1[times_id[1]] , ((H_1[times_id])[2:n_times] - (H_1[times_id])[1:(n_times-1)]))
+
+        H_all = cumsum(N_all)/noobs
+        H_all_dif = c(H_all[times_id[1]] , ((H_all[times_id])[2:n_times] - (H_all[times_id])[1:(n_times-1)]))
+
+        H_lag = Y_lag / noobs
+        H_lag_inv = 1 / H_lag
+        H_lag_inv[is.na(H_lag_inv)] <- 0
+
+
+        Lambda_0_dif = H_0_dif / H_lag
+        Lambda_1_dif = H_1_dif / H_lag
+        Lambda_all_dif = H_all_dif / H_lag
+        Lambda_0_dif_comp_inv = 1 / (1 - Lambda_0_dif)
+        Lambda_0_dif_comp_inv[is.na(Lambda_0_dif_comp_inv)] <- 0
+
+        S = exp(cumsum(log(1 - Lambda_all_dif)))
+        S[is.na(S)] <- 0
+        S_lag = c(1 , S[1:(n_times-1)])
+
+        G = exp(cumsum(log(1 - Lambda_0_dif)))
+        G[is.na(G)] <- 0
+        G_lag = c(1 , G[1:(n_times-1)])
+        G_lag_inv = 1 / G_lag
+        G_lag_inv[is.na(G_lag_inv)] <- 0
+
+        F_1 = cumsum(S_lag * Lambda_1_dif)
+
+        d_phi_1 = c((N_1 * G_lag_inv[times_nr]) , rep(0, noobs - len2))
+        d_phi_2 = c(N_0 * (F_1[n_times] - F_1[times_nr]) * Lambda_0_dif_comp_inv[times_nr] * H_lag_inv[times_nr] -
+                        cumsum(H_0_dif * (F_1[n_times] - F_1) * Lambda_0_dif_comp_inv * H_lag_inv^2)[times_nr] ,
+                    rep(-sum(H_0_dif * (F_1[n_times] - F_1) * Lambda_0_dif_comp_inv * H_lag_inv^2), noobs-len2))
+
+        beta = object$coefficients
+        k = length(beta)
+
+        z = object$x[datord,]  ## what if the model contains po at multiple time points?
+
+        muhat = object$family$linkinv(z %*% beta)
+        Ahat = z * as.vector(object$family$mu.eta(z %*% beta))
+        Ahat_red = Ahat[1:len2,]
+        mu_derivhat = z * as.vector(object$family$mu.eta(z %*% beta))
+
+        a_len <- a1_len <- a2_len <- a3_len <-
+            b_1 <- b_2 <- b_3 <- b_4 <- b_5 <- b_6 <-
+            matrix(NA, nrow = noobs, ncol = k)
+        H_0z <- H_0z_dif <- H_1z <- H_1z_dif <- H_z <- H_z_lag <- matrix(NA, nrow = len2, ncol = k)
+
+
+        ## compute variance
+        for(j in 1:k) {
+            a_len[,j] = Ahat[,j] * (d_phi_1-muhat+d_phi_2)
+            a1_len[,j] = Ahat[,j]  * (d_phi_1)
+            a2_len[,j] = Ahat[,j] * (-muhat)
+            a3_len[,j] = Ahat[,j] * (d_phi_2)
+
+            H_0z[,j] = cumsum(Ahat_red[,j] * N_0)/noobs
+            H_0z_dif[,j] = c(H_0z[times_id[1],j] , ((H_0z[times_id,j])[2:n_times] - (H_0z[times_id,j])[1:(n_times-1)]))
+
+            H_1z[,j] = cumsum(Ahat_red[,j] * N_1)/noobs
+            H_1z_dif[,j] = c(H_1z[times_id[1],j] , ((H_1z[times_id,j])[2:n_times] - (H_1z[times_id,j])[1:(n_times-1)]))
+
+            H_z[,j] = (sum(Ahat[,j]) - cumsum(Ahat_red[,j]))/noobs
+            H_z_lag[,j] = c(sum(Ahat[,j]) / noobs , (H_z[1:(len2-1),j])[times_id[1:(n_times-1)]])
+
+            temp1 = cumsum( H_0z_dif[,j] * H_lag_inv * Lambda_0_dif_comp_inv - H_z_lag[,j] * H_lag_inv^2 * Lambda_0_dif_comp_inv * H_0_dif )
+            temp1_lag = c(0 , temp1[1:(n_times-1)])
+            temp1_dif = temp1 - temp1_lag
+
+            temp2 = cumsum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv * Lambda_0_dif_comp_inv -
+                                H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv  * H_0_dif )
+            temp3 = cumsum( H_1z_dif[,j] / G_lag )
+
+
+            b_1[,j] = c(N_1 * G_lag_inv[times_nr] * temp1_lag[times_nr] , rep(0, len-len2))
+
+            b_2[,j] = c(N_0 * (temp3[n_times] - temp3[times_nr]) * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
+                            cumsum( H_0_dif * (temp3[n_times] - temp3) * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
+                        rep(-sum( H_0_dif * (temp3[n_times] - temp3) * H_lag_inv^2 * Lambda_0_dif_comp_inv ), len-len2))
+
+            temp4 = cumsum( H_1_dif * G_lag_inv * temp1 )
+
+            b_3[,j] = c(N_0 * (temp4[n_times] - temp4[times_nr]) * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
+                            cumsum( H_0_dif * (temp4[n_times] - temp4) * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
+                        rep(-sum( H_0_dif * (temp4[n_times] -temp4) / H_lag^2 / (1-Lambda_0_dif) ), len-len2 ))
+
+
+            b_4[,j] = c(- N_0 * (F_1[n_times] - F_1[times_nr]) * H_z_lag[times_nr,j] * H_lag_inv[times_nr]^2 * Lambda_0_dif_comp_inv[times_nr] +
+                            cumsum( H_0_dif * (F_1[n_times] - F_1) * H_z_lag[,j] * H_lag_inv^3 * Lambda_0_dif_comp_inv )[times_nr] ,
+                        rep(sum( H_0_dif * (F_1[n_times] - F_1) * H_z_lag[,j] * H_lag_inv^3 * Lambda_0_dif_comp_inv ), len-len2 ))
+
+
+            b_5[,j] = c(-cumsum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv -
+                                     H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^3 * Lambda_0_dif_comp_inv * H_0_dif )[times_nr] ,
+                        rep(- sum( H_0z_dif[,j] * (F_1[n_times] - F_1) * H_lag_inv^2 * Lambda_0_dif_comp_inv -
+                                       H_z_lag[,j] * (F_1[n_times] - F_1) * H_lag_inv^3 * Lambda_0_dif_comp_inv * H_0_dif ), len-len2 ))
+
+            b_6[,j] = c(N_0 * (F_1[n_times] - F_1[times_nr]) * temp1_dif[times_nr] * H_lag_inv[times_nr] * Lambda_0_dif_comp_inv[times_nr] -
+                            cumsum( H_0_dif * (F_1[n_times] - F_1) * temp1_dif * H_lag_inv^2 * Lambda_0_dif_comp_inv )[times_nr] ,
+                        rep(-sum( H_0_dif * (F_1[n_times] - F_1) * temp1_dif * H_lag_inv^2 * Lambda_0_dif_comp_inv ), len-len2 ))
+
+
+        }
+
+        b <- b_1 + b_2 + b_3 + b_4 + b_5 + b_6
+        Sigma <- t(a_len) %*% a_len / noobs + t(a_len) %*% b / noobs +
+            t(b) %*% a_len / noobs + t(b) %*% b / noobs
+
+
+        Minvhat <- solve(t(Ahat) %*% mu_derivhat / nrow(Ahat))
+
+        Minvhat %*% Sigma %*% t(Minvhat) / noobs
+
+
+    } else if(type == "robust") {
+
+        class(object) <- c("glm", "lm")
+        sandwich::sandwich(object)
+
+    } else if(type == "naive") {
+
+        class(object) <- c("glm", "lm")
+        stats::vcov(object)
+
+
+    } else stop("unknown variance type")
 
 }
 
@@ -245,6 +277,7 @@ vcov.pseudoglm <- function(object, complete = TRUE, ...) {
 #' @export
 #'
 summary.pseudoglm <- function (object, correlation = FALSE, symbolic.cor = FALSE,
+                               type = "corrected",
                                ...)
 {
     df.r <- object$df.residual
@@ -254,7 +287,7 @@ summary.pseudoglm <- function (object, correlation = FALSE, symbolic.cor = FALSE
         p1 <- 1L:p
         Qr <- object$qr
         coef.p <- object$coefficients[Qr$pivot[p1]]
-        covmat.unscaled <- object$corrected.vcov[p1, p1]
+        covmat.unscaled <- vcov(object, type = type)[p1, p1]
         dimnames(covmat.unscaled) <- list(names(coef.p), names(coef.p))
         covmat <- covmat.unscaled
         var.cf <- diag(covmat)
--- tests/scratch.R
+++ tests/scratch.R
@@ -44,3 +44,37 @@ for(i in 1:nsim) {
 sqrt(var(estbet.z))
 mean(estvar.z)
 
+## comparison of variance types
+
+
+nsim <- 1000
+estvar <- as.data.frame(matrix(NA, nrow = nsim * 3, ncol = 3))
+estbet<- matrix(NA, nrow = nsim, ncol = 2)
+
+j <- 1
+for(i in 1:nsim) {
+    test <- gen_data(n=200)
+
+    fit <- cumincglm(formula = Hist(obsT, indi) ~ Z,
+                     time = 1, cause = "1", link = "log", data = test)
+
+    estvar[j,] <- c(sqrt(diag(vcov(fit, type = "corrected"))), 1)
+    estvar[j + 1, ] <- c(sqrt(diag(vcov(fit, type = "robust"))), 2)
+    estvar[j + 2, ] <- c(sqrt(diag(vcov(fit, type = "naive"))), 3)
+
+    j <- j + 3
+
+    estbet[i,] <- fit$coefficients
+
+}
+
+colnames(estvar) <- c("intercept", "Z", "type")
+
+sqrt(diag(cov(estbet)))
+
+library(dplyr)
+estvar %>% group_by(type) %>% summarize(mint = mean(intercept), mz = mean(Z))
+
+
+library(ggplot2)
+ggplot(estvar, aes(x = factor(type), y = Z)) + geom_boxplot()
